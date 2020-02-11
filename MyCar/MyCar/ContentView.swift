e9016be7abbaabb87453444498ccdc5d84659111
radude89 <radu.ionut.dan@gmail.com>
2020-02-11 07:01:13 +0200

    - Base setup, added project.

---
+++ MyCar/MyCar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCar
+//
+//  Created by Radu Dan on 11/02/2020.
+//  Copyright © 2020 Radu Dan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
